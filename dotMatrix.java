1ce5ba38ab3d12c54854c7962dc4b1ab5c93026e
edicson345 <edicson345@gmail.com>
2017-03-13 13:56:33 -0400

    Update dotMatrix.java

--- dotMatrix.java
+++ dotMatrix.java
@@ -1,7 +1,6 @@
 /*
-This is probably a graph problem which I'm still learning about so the O(n^2) solution I made is a pretty mediocre attempt to produce 
-some kind of result. The brute force way this was done was to visit the entire 4x4 matrix searching for '1's and printing out the row 
-and column. I will probably revisit this problem with a better solution after learning more about graph theory.
+This is probably a graph problem which I'm still learning about so the O(n^2) solution is an attempt at a trivial solution. 
+I will probably revisit this problem with a better solution after learning more about graph theory.
 */
 
 public class dotMatrix
