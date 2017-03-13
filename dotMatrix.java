813864b60271388e6b05d86cb1b83818b5887bd3
edicson345 <edicson345@gmail.com>
2017-03-13 11:13:40 -0400

    second commit

--- dotMatrix.java
+++ dotMatrix.java
@@ -8,23 +8,14 @@ public class dotMatrix
                          {1,0,0,0},
                          {0,0,0,0}};
 
-    boolean[][] hasNeighbor = new boolean[adjMatrix.length][adjMatrix[0].length];
-
     for(int i = 0; i < adjMatrix.length; i++)
       for(int j = 0; j < adjMatrix[i].length; j++)
       {
         if(adjMatrix[i][j] == 1)
         {
-          hasNeighbor[i][j] = true;
+          System.out.println(i + "," + j);
         }
       }
-
-    for(int i = 0; i < hasNeighbor.length; i++)
-        for(int j = 0; j < hasNeighbor[i].length; j++)
-        {
-          if(hasNeighbor[i][j])
-            System.out.println(i + "," + j);
-        }
   }
 
   public static void main(String[] args)
